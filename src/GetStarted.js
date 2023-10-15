40f3028abeb57333193332f305b9bfabc46cffbe
mbagleyradio <mbagleyradio@gmail.com>
2023-10-15 10:24:20 -0700

    Starting the Insurance Selection page

--- src/GetStarted.js
+++ src/GetStarted.js
@@ -1,5 +1,17 @@
 function GetStarted() {
+    return(
+        <>
+            <div id="gs-header">
 
+            </div>
+            <div id="gs-buttons">
+
+            </div>
+            <div id="gs-options-box">
+                
+            </div>
+        </>
+    );
 }
 
 export default GetStarted;
