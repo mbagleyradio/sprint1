0472ff59f5c5ce0e8cc4d6afbc9f16df67433940
mbagleyradio <mbagleyradio@gmail.com>
2023-10-16 10:45:44 -0700

    Aligned buttons and built horizontal line break

--- src/GetStarted.js
+++ src/GetStarted.js
@@ -12,13 +12,19 @@ function GetStarted() {
                 <button id="comInsuranceBtn">Commercial Insurance</button>
                 <button id="noInsuranceBtn">NO Insurance</button>
                 <button id="medicareBtn">Medicare</button>
+                <br/>
                 <button id="medAdvantageBtn">Medicare Advantage, HMO, PPO</button>
                 <button id="medicaidBtn">Medicaid</button>
                 <button id="milInsuranceBtn">Military Insurance</button>
                 <button id="medManagedBtn">Medicaid Managed Care (MCO)</button>
+                <br/>
                 <button id="intInsuranceBtn">International Insurance</button>
                 <button id="workCompBtn">Workers Comp</button>
                 <button id="behavioralBtn">Behavioral Health Ins.</button>
+            </div>
+            <div id="gs-line-break">
+                <hr></hr>
+                <button id="yourBtn">Your Selection</button>
             </div>
             <div id="gs-options-box">
                 
