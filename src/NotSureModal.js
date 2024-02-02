b95cd6348b66201aaf74e2d3136ed452348f373c
mbagleyradio <mbagleyradio@gmail.com>
2024-02-02 15:41:26 -0800

    imported microphone pic

--- src/NotSureModal.js
+++ src/NotSureModal.js
@@ -1,14 +1,18 @@
 import './NotSureModal.css';
 import ATC_Assistant from './sprint2/img/ATC Assistant 2.png';
+import mic from './sprint2/img/mic.png';
+
 function NotSureModal({ onSubmit, onClose }) {
     return (
         <div id="backgroundWithModalOpen">
             <div id="modalWindow">
                 <div id="modalCloseButton">
+                    <img id="mic" src={mic} alt="Click this button to speak to the chat assistant with your device's microphone"/>
                     <button id="closeBtn" onClick={() => onClose("Close button clicked in modal")}>Close</button>
                 </div>
                 <div id="modalContent">
-                    <h1>This is the modal title</h1>
+                    <label for="chatWindow">Chat Window</label>
+                    <textarea id="chatWindow" name="chatWindow" rows="4" cols="64" placeholder="Enter text here..."></textarea>
                     <img src={ATC_Assistant} alt="ATC Assistant"/><br/>
                     <button id="submitBtn" onClick={() => onSubmit("Submit button clicked in modal")}>Submit</button>
                 </div>
