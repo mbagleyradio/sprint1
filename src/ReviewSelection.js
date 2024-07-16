bfdb720d813b403883da56fededd5d72e0bf97e0
mbagleyradio <mbagleyradio@gmail.com>
2024-07-15 21:27:09 -0700

    got it!!!

--- src/ReviewSelection.js
+++ src/ReviewSelection.js
@@ -5,7 +5,8 @@ import { useState, useRef } from 'react';
 import A2CLogo from './A2CLogo_150x150.png';
 
 function ReviewSelection() {
-    const [ submittedFilters, setSubmittedFilters ] = useState([]);
+    const submittedFilters = useRef([]); 
+    //const [ submittedFilters, setSubmittedFilters ] = useState([]);
     let uniqueFilterID = useRef(0);
 
     // acquiring data from previous page (insurance type & insurance name), and sanitizing that data for display on this page
@@ -15,17 +16,31 @@ function ReviewSelection() {
 
     // setter is not working right ... could you count ID with the .length function? ID would equal .length - 1. For removal, you would just need to splice based on 3 conditional lengths (first, middle, end)
     const handleFilterSubmission = (submission) => {
-        setSubmittedFilters([...submittedFilters, {
+        /*setSubmittedFilters([...submittedFilters, {
             id: uniqueFilterID,
             filterName: submission.filterName 
-        }]);
+        }]); */
+
+        submittedFilters.current.push({
+            id: uniqueFilterID,
+            filterName: submission.filterName
+        });
 
         uniqueFilterID.current = uniqueFilterID.current + 1;
     }
 
     const handleFilterRemoval = (selection) => {        
+        if (selection === submittedFilters.current[0].filterName) {
+            submittedFilters.current = submittedFilters.current.filter(elementOfFiltersArray => elementOfFiltersArray.filterName === "");
+        } else {
+            submittedFilters.current = submittedFilters.current.filter(elementOfFiltersArray => elementOfFiltersArray.filterName !== selection);
+        }
+        
         uniqueFilterID.current = uniqueFilterID.current - 1;
-        setSubmittedFilters(submittedFilters.filter(elementOfFiltersArray => elementOfFiltersArray.filterName !== selection));
+       
+        console.log(submittedFilters.current);
+
+        // you need to handle what will happen if the FIRST element in the array gets undone, because right now the UI nukes all of that but the queries remain
     }
 
     const handleReviewListClick = () => {
@@ -50,7 +65,7 @@ function ReviewSelection() {
                 <p className="reviewSelectionText">CATEGORY: {listingToReview.healthCareCategory}</p>
             </div>
             <div id="filterHealthCareScreen">
-                <FilterHealthCareSelection insuranceType={listingToReview.insuranceType} insuranceName={listingToReview.insuranceName} healthCareCategory={listingToReview.healthCareCategory} addFilters={handleFilterSubmission} collectedFilters={submittedFilters} removeFilters={handleFilterRemoval}/>
+                <FilterHealthCareSelection insuranceType={listingToReview.insuranceType} insuranceName={listingToReview.insuranceName} healthCareCategory={listingToReview.healthCareCategory} addFilters={handleFilterSubmission} collectedFilters={submittedFilters.current} removeFilters={handleFilterRemoval}/>
             </div>
             <div id="reviewListBtn">
                 <button type="button" id="reviewSelectionBtn" onClick={handleReviewListClick}>REVIEW LIST</button>
