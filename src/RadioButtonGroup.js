f23dca579b94a0a08500b062a9780910cea98218
mbagleyradio <mbagleyradio@gmail.com>
2025-04-29 19:16:41 -0700

    fixed other bugs ... realized I need to refactor radiobuttongroup for checkboxes

--- src/RadioButtonGroup.js
+++ src/RadioButtonGroup.js
@@ -4,7 +4,7 @@ import './RadioButtonGroup.css';
 export default function RadioButtonGroup({insurances}) {
 	const navigate = useNavigate();
 	const [ selection, setSelection ] = useState("");
-	
+
 	function handleSubmit(event) {
 		event.preventDefault();
 		if (selection.includes("not") && selection.includes("listed")) {
@@ -13,7 +13,13 @@ export default function RadioButtonGroup({insurances}) {
 			navigate("../healthcare-categories", {state: selection});
 		}
 	}
-	return(
+
+	function appendSelection(event) {
+		// not sure
+	}
+	
+	if (insurances[0].insurance_type !== "Uninsured / Self-Pay") { 
+		return(
         <form id="insuranceSelectForm" onSubmit={handleSubmit}>
 			<button id="submitFromRadioBTN" type="submit">Submit</button><br/>
 			<div className="radioButtonMenu">
@@ -44,4 +50,37 @@ export default function RadioButtonGroup({insurances}) {
 			</div>
 		</form>
     );
+	} else {
+		return (
+		<form id="insuranceSelectForm" onSubmit={handleSubmit}>
+			<button id="submitFromRadioBTN" type="submit">Submit</button><br/>
+			<div className="radioButtonMenu">
+				{insurances.map((element) => (
+					<div className=""id={`radioDiv_${element.id_in_group}`}>
+					<input
+						type="checkbox"
+						value={element.insurance_name}
+						id={`button_${element.id_in_group}`}
+						name="insGroup"
+						onChange={appendSelection} // need to fix
+					/>
+					<label className="radioButtonText" for={`button_${element.id_in_group}`}>
+						{element.insurance_name}
+					</label>
+					<br />
+					</div>
+				))}
+			</div>
+			<div>
+				<input type="radio" 
+					id="notListed" 
+					name="insGroup"
+					value={`${insurances[0].insurance_type}: my insurance is not listed.`}
+					onChange={e => {setSelection(e.target.value)}}
+				/>
+				<label className="radioButtonText" for="notListed">My insurance is not listed.</label>
+			</div>
+		</form>
+		)
+	}
 }
